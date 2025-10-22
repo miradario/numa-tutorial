2debd3e2a0c258abc03062d41e1658932ec705e9
Dario Mira <miradario@gmail.com>
2025-10-22 09:18:53 -0300

    home protected

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,42 @@
 import { Stack } from "expo-router";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
+
+function RootLayoutWithAuth() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return null; // or a loading indicator
+  }
+
+  return (
+    <Stack>
+      <Stack.Protected
+        guard={isSignedIn}
+      >
+      <Stack.Screen
+        name="(protected)"
+      />
+      </Stack.Protected>
+      <Stack.Protected
+        guard={!isSignedIn}
+      >
+      <Stack.Screen
+        name="(public)"
+        />
+      </Stack.Protected>
+    </Stack>
+  );
+}
 export default function RootLayout() {
-  return <Stack />;
+  
+  return (
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <RootLayoutWithAuth />
+    </ClerkProvider>
+  );
 }
